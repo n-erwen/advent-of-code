226e5f35cb4f4969f10bcdaacce659849334e89e
n-erwen <n.erwen@icloud.com>
2020-12-22 20:18:11 +0000

    (2020) Day 3 Solved

---
+++ Day 3/part1.py
@@ -0,0 +1,25 @@
+def move_down_slope(x,y,pattern_len):
+    x += 3
+    if x > pattern_len - 1:
+        x -= pattern_len
+    y += 1
+    return [x,y]
+
+def is_square_open_or_tree(pos):
+    if pos == "#":
+        return "X"
+    elif pos == ".":
+        return "O"
+
+input_file = open("input.txt", "r")
+toboggan_map = input_file.read().split("\n")
+input_file.close()
+map_pattern_length = len(toboggan_map[0])
+x = 0
+y = 0
+
+while y < len(toboggan_map)-1:
+    x,y = move_down_slope(x,y,map_pattern_length)
+    toboggan_map[y] = toboggan_map[y][:x] + is_square_open_or_tree(toboggan_map[y][x]) + toboggan_map[y][x:]
+
+print("Part 1: " + str("\n".join(toboggan_map).count("X")))
---
+++ Day 3/part2.py
@@ -0,0 +1,45 @@
+def move_down_slope(slope, pos, pattern_len):
+    pos["x"] += slope["right"]
+    if pos["x"] > pattern_len - 1:
+        pos["x"] -= pattern_len
+    pos["y"] += slope["down"]
+    return {"x": pos["x"], "y": pos["y"]}
+
+def is_square_open_or_tree(pos):
+    if pos == "#":
+        return "X"
+    elif pos == ".":
+        return "O"
+
+def num_of_trees_on_slope(slope, t_map, pattern_len):
+    pos = {"x": 0, "y": 0}
+    while pos["y"] < len(t_map)-1:
+        pos = move_down_slope(slope, pos, pattern_len)
+        if pos["y"] > len(t_map) - 1:
+            break
+        t_map[pos["y"]] = t_map[pos["y"]][:pos["x"]] + is_square_open_or_tree(t_map[pos["y"]][pos["x"]]) + t_map[pos["y"]][pos["x"]:]
+
+    return "\n".join(t_map).count("X")
+
+input_file = open("input.txt", "r")
+toboggan_map = input_file.read().split("\n")
+input_file.close()
+map_pattern_length = len(toboggan_map[0])
+
+slopes = [
+    {"right": 1, "down": 1},
+    {"right": 3, "down": 1},
+    {"right": 5, "down": 1},
+    {"right": 7, "down": 1},
+    {"right": 1, "down": 2},
+]
+
+trees_on_each_slope = []
+for s in slopes:
+    trees_on_each_slope.append(num_of_trees_on_slope(s, toboggan_map[:], map_pattern_length))
+
+product = 1
+for trees in trees_on_each_slope:
+    product *= trees
+
+print("Part 2: " + str(product))
