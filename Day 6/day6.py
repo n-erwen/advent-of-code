6f68d0b27cbc38bdf278f5b8bc72cc678f01b83e
n-erwen <n.erwen@icloud.com>
2020-12-29 23:52:04 +0000

    (2020) Day 6 Solved

--- Day 6/day6.py
+++ Day 6/day6.py
@@ -1,3 +1,13 @@
+def did_everyone_answer_question(question, responses):
+    return all([question in response for response in responses])
+
+def count_questions_everyone_answered(questions_answered, responses):
+    questions_everyone_answered = []
+    for q in questions_answered:
+        if did_everyone_answer_question(q, responses):
+            questions_everyone_answered.append(q)
+    return len(questions_everyone_answered)
+
 input_file = open("input.txt", "r")
 all_responses = input_file.read().split("\n\n")
 input_file.close()
@@ -5,9 +15,21 @@ input_file.close()
 responses_by_group = [a.split("\n") for a in all_responses]
 
 # Part 1
-num_of_questions_answered_by_group = []
-for group in responses_by_group:
-    questions_answered = set([question for answers in group for question in answers])
-    num_of_questions_answered_by_group.append(len(questions_answered))
+questions_answered_per_group = [
+    set([question for answers in group for question in answers]) 
+    for group in responses_by_group
+]
     
-print("(Day 6: Part 1) Result: " + str(sum(num_of_questions_answered_by_group)))
+print("(Day 6: Part 1) Result: " + str(sum([len(q) for q in questions_answered_per_group])))
+
+# Part 2
+
+num_questions_everyone_answered_per_group = [
+    count_questions_everyone_answered(
+        questions_answered_per_group[index],
+        group
+    ) 
+    for index, group in enumerate(responses_by_group)
+]
+
+print("(Day 6: Part 2) Result: " + str(sum(num_questions_everyone_answered_per_group)))
