76993adba0496c3edcad50bc895c119a407f8fbb
n-erwen <n.erwen@icloud.com>
2021-11-24 11:31:06 +0000

    (2020) Day 7 Part 2 Solved

--- 2020/Day 7/day7.py
+++ 2020/Day 7/day7.py
@@ -1,4 +1,3 @@
-
 import re
     
 def load_input(file_path):
@@ -9,7 +8,7 @@ def parse_rules(bag_rules):
     def parse_contained_bag(bag):
         regex = r"([1-9]+) (.*) bags?"
         quantity, color = re.search(regex, bag).groups()
-        return {"quantity": quantity, "color": color}
+        return {"quantity": int(quantity), "color": color}
     
     rules_dict = {}
     for r in bag_rules:
@@ -19,7 +18,7 @@ def parse_rules(bag_rules):
         else:
             rules_dict[color]= [parse_contained_bag(b) for b in contained_bags.rstrip(".").split(", ")]
     return rules_dict
-        
+
 # Part 1
 def can_color_contain_shiny_gold_bag(color, rules):
     for b in rules[color]:
@@ -30,7 +29,20 @@ def can_color_contain_shiny_gold_bag(color, rules):
 def colors_containing_shiny_gold_bag(rules):
     return [color for color in rules.keys() if can_color_contain_shiny_gold_bag(color, rules)]
 
+# Part 2
+
+def total_num_bags(bags):
+    return sum([b["quantity"] for b in bags])
+
+def how_many_bags_must_color_contain(color,rules):
+    return total_num_bags(rules[color]) + \
+        sum([how_many_bags_must_color_contain(b["color"], rules)*b["quantity"] for b in rules[color]])
+
+
 rules = parse_rules(load_input('./input.txt'))
 print("(Day 7 Part 1): {} different colored bags can eventually contain a shiny gold bag.".format(
     len(colors_containing_shiny_gold_bag(rules)))
 )
+print("(Day 7 Part 2): A single shiny bag must contain {} other bags.".format(
+    how_many_bags_must_color_contain("shiny gold", rules)
+))
