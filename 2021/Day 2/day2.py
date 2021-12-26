eabd139b87e328c9a5fca00b2aef9260a8fa1f6e
n-erwen <n.erwen@icloud.com>
2021-12-26 11:50:22 +0000

    (2021) Day 2 Solved

--- 2021/Day 2/day2.py
+++ 2021/Day 2/day2.py
@@ -2,10 +2,11 @@ def load_input(file_path):
     with open(file_path, "r") as f:
         return f.read().split("\n")
 
-def total_distance_travelled_in_direction(direction, cmds):
+def total_distance_travelled_in_direction(direction, cmds, end=-1):
+    end = len(cmds) if end == -1 else end
     return sum([
-        int(c.split(" ")[1]) for c in cmds 
-        if c.split(" ")[0] == direction
+        int(cmds[i].split(" ")[1]) for i in range(0,end) 
+        if cmds[i].split(" ")[0] == direction
     ])
 
 def get_final_horizontal_position(cmds):
@@ -15,34 +16,27 @@ def get_final_depth(cmds):
     return total_distance_travelled_in_direction("down", cmds) - total_distance_travelled_in_direction("up", cmds)
 
 def get_final_aimed_depth(cmds):
-    final_depth = 0
-    for c in cmds:
-        direction, amount = c.split(" ")[0], int(c.split(" ")[1])
-        if direction == "up":
-            final_depth += amount
-        elif direction == "down":
-            final_depth -= amount
-        elif direction == "forward":
-            final_depth += amount*final_depth
-    return final_depth
+    return sum([
+        int(cmds[i].split(" ")[1]) * (
+            total_distance_travelled_in_direction("down", cmds, end=i) - total_distance_travelled_in_direction("up", cmds, end=i)
+        ) for i in range(0, len(cmds)) 
+        if cmds[i].split(" ")[0] == "forward"
+    ])
 
-IS_EXAMPLE = True
-INPUT_FILE_PATH = './example_input.txt' if IS_EXAMPLE else './input.txt'
+INPUT_FILE_PATH = './input.txt'
 
 if __name__ == "__main__":
     commands = load_input(INPUT_FILE_PATH)
     # PART 1
     def part1():
         return get_final_depth(commands) * get_final_horizontal_position(commands)
-    part1_result = part1()
-    print("(Day 2 Part 1) Final horizontal position * final depth = {}".format(part1_result))
-    if IS_EXAMPLE:
-        assert part1_result == 150, "Expected 150, received {}".format(part1_result)
+    print("(Day 2 Part 1) Final horizontal position * final depth = {}".format(
+        part1()
+    ))
     
     # PART 2
     def part2():
         return get_final_aimed_depth(commands) * get_final_horizontal_position(commands)
-    part2_result = part2()
-    print("(Day 2 Part 2) Final horizontal position * final depth = {}".format(part2_result))
-    if IS_EXAMPLE:
-        assert part2_result == 900, "Expected 900, received {}".format(part2_result)
+    print("(Day 2 Part 2) Final horizontal position * final depth = {}".format(
+        part2()
+    ))
