f20db2c59f7ce5569df344f94a91b7f9866a7d0a
n-erwen <n.erwen@icloud.com>
2022-12-01 20:41:16 +0000

    2022 Day 1 Refactor

--- 2022/Day 1/day1.py
+++ 2022/Day 1/day1.py
@@ -10,20 +10,21 @@ def split_inventories(input_string: str) -> List[List[int]]:
     return [[int(calories) for calories in inv.split("\n")] for inv in input_string.split("\n\n")]
 
 
-def part1_solution(input_string: str) -> int:
+def get_calorie_counts(inventories: List[List[int]]) -> List[int]:
+    return [sum(inv) for inv in inventories]
+
+
+def part1_solution(inventories: List[List[int]]) -> int:
     """Part 1: Total calories the elf with the most calories is carrying"""
-    calorie_counts = [sum(inv) for inv in split_inventories(input_string)]
-    return max(calorie_counts)
+    return max(get_calorie_counts(inventories))
 
 
-def part2_solution(input_string: str) -> int:
+def part2_solution(inventories: List[List[int]]) -> int:
     """Part 2: Sum of calories the top three elves are carrying"""
-    calorie_counts = [sum(inv) for inv in split_inventories(input_string)]
-    top_three_calories = sorted(calorie_counts, reverse=True)[:3]
-    return sum(top_three_calories)
+    return sum(sorted(get_calorie_counts(inventories), reverse=True)[:3])
 
 
 if __name__ == "__main__":
-    input_string = load_puzzle_input("./input.txt")
-    print(f"Part 1 Solution: {part1_solution(input_string)}")
-    print(f"Part 2 Solution: {part2_solution(input_string)}")
+    inventories_input = split_inventories(load_puzzle_input("./input.txt"))
+    print(f"Part 1 Solution: {part1_solution(inventories_input)}")
+    print(f"Part 2 Solution: {part2_solution(inventories_input)}")
