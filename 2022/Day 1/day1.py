b6d606025d3e5238b5adf55a204c13b3ec032087
n-erwen <n.erwen@icloud.com>
2022-12-01 19:51:50 +0000

    2022 Day 1 Solved

---
+++ 2022/Day 1/day1.py
@@ -0,0 +1,24 @@
+from typing import List
+
+
+def split_inventories(input_string: str) -> List[List[int]]:
+    return [[int(calories) for calories in inv.split("\n")] for inv in input_string.split("\n\n")]
+
+
+def part1_solution(input_string: str) -> int:
+    """Part 1: Total calories the elf with the most calories is carrying"""
+    calorie_counts = [sum(inv) for inv in split_inventories(input_string)]
+    return max(calorie_counts)
+
+
+def part2_solution(input_string: str) -> int:
+    """Part 2: Sum of calories the top three elves are carrying"""
+    calorie_counts = [sum(inv) for inv in split_inventories(input_string)]
+    top_three_calories = sorted(calorie_counts, reverse=True)[:3]
+    return sum(top_three_calories)
+
+
+if __name__ == "__main__":
+    input_string = open("./input.txt", "r").read().rstrip()
+    print(f"Part 1 Solution: {part1_solution(input_string)}")
+    print(f"Part 2 Solution: {part2_solution(input_string)}")
---
+++ 2022/Day 1/day1_test.py
@@ -0,0 +1,29 @@
+import unittest
+import day1
+
+SAMPLE_INPUT = """1000
+2000
+3000
+
+4000
+
+5000
+6000
+
+7000
+8000
+9000
+
+10000"""
+
+
+class TestDay1(unittest.TestCase):
+    def test_part1(self):
+        self.assertEqual(day1.part1_solution(SAMPLE_INPUT), 24000)
+
+    def test_part2(self):
+        self.assertEqual(day1.part2_solution(SAMPLE_INPUT), 45000)
+
+
+if __name__ == '__main__':
+    unittest.main()
