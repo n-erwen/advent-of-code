7161207dfcb5e7111975850ae6a432c29bc29fb8
n-erwen <n.erwen@icloud.com>
2024-04-21 02:07:41 +0100

    [2022] Day 7 WIP 2

--- 2022/Day 7/day7.py
+++ 2022/Day 7/day7.py
@@ -1,18 +1,36 @@
+import re
+
+
+class Folder:
+    def __init__(self, parent: "Folder" or None, name=""):
+        self.name = name
+        self.parent = parent
+        self.children = []
+
+
+class File:
+    def __init__(self, size: int, folder: Folder, name=""):
+        self.name = name
+        self.size = size
+        self.folder = folder
+
+
 def load_puzzle_input(file_name: str) -> str:
     with open(file_name, "r") as file:
         return file.read().rstrip()
 
 
-def generate_file_tree(command_output: str):
-    command_strings = command_output.lstrip("$ ").split("$ ")
-    for command_string in command_strings:
-        print(command_string)
-        print("\n")
+def generate_file_tree(terminal_output: str):
+    # file_tree = Folder(None, "/")
+    # current_node = file_tree
+    # print(terminal_output)
+    sections = re.findall(r"^\$([\s\S]*?)(?=\$)", terminal_output)
+    print(sections)
     return None
 
 
 def part1_solution(puzzle_input: str) -> None:
-    file_tree = generate_file_tree(puzzle_input)
+    generate_file_tree(puzzle_input)
     return None
 
 
