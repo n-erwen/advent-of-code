c458971e8aaab4dd145bc725a7cfcc39da4ae565
n-erwen <n.erwen@icloud.com>
2024-07-15 12:44:33 +0100

    (2020) Day 9 Part 1 Solved

--- 2020/Day 9/day9.py
+++ 2020/Day 9/day9.py
@@ -1,27 +1,29 @@
-def is_sum_of_two_nums_in_array(num, arr):
-    for possible_factor in arr:
-        if num - possible_factor in arr:
-            return True
-    return False
-
-
-def day9(data, preamble_size):
-    for n in range(5, len(data)):
-        prev_numbers = data[n-preamble_size:n]
-        if data[n] == 20:
-            pass
-        if not is_sum_of_two_nums_in_array(data[n], prev_numbers):
-            return data[n]
-    return 0
-
-
 def load_input(file_path):
     with open(file_path, "r") as file:
         return file.read().split("\n")
 
+
 def transform_all_items_to_integers(arr):
     return [int(i) for i in arr]
 
+
+def get_first_num_not_sum_of_prev_25_nums(num_array, preamble_size):
+    for i in range(preamble_size, len(num_array)):
+        prev_nums_array = num_array[i - preamble_size:i]
+        if not any([
+            (num_array[i] - prev_num != num_array[i]
+             and num_array[i] - prev_num in prev_nums_array)
+            for prev_num in prev_nums_array
+        ]):
+            return num_array[i]
+
+
+def part1_solution(input, preamble_size):
+    return get_first_num_not_sum_of_prev_25_nums(
+        transform_all_items_to_integers(input), preamble_size
+    )
+
+
 if __name__ == "__main__":
     puzzle_input = transform_all_items_to_integers(load_input('./input.txt'))
-    print(day9(puzzle_input, 25))
+    print('Day 1 Part 1 Answer: ' + str(part1_solution(puzzle_input, 25)))
--- 2020/Day 9/day9_test.py
+++ 2020/Day 9/day9_test.py
@@ -1,15 +1,15 @@
 import unittest
-from day9 import day9, transform_all_items_to_integers
+import day9
 
-SAMPLE_INPUT = transform_all_items_to_integers([
+SAMPLE_INPUT = [
     '35', '20', '15', '25', '47', '40', '62', '55', '65', '95',
     '102', '117', '150', '182', '127', '219', '299', '277', '309', '576'
-])
+]
 
 
 class TestDay9(unittest.TestCase):
     def test_sample_input(self):
-        self.assertEqual(day9(SAMPLE_INPUT, 5), 127)
+        self.assertEqual(day9.part1_solution(SAMPLE_INPUT, 5), 127)
 
 
 if __name__ == '__main__':
